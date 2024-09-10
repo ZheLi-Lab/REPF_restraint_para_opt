5f113be1ecbd1aac53760003b9cd23a9b90fce09
zlisysu <lizhe5@mail.sysu.edu.cn>
2024-09-10 14:54:07 +0800

    modi_

--- utils/prev_backup/Restraints_Select_Mobley_BACE_ver/Selection_by_Mobley_BACE_lig13.py
+++
@@ -1,7 +0,0 @@
-class Sel_rest_atm_Mobley_based():
-    def __init__(self, lig):
-        if lig == "lig_13":
-            self.res_lst=[[ 6060, 6062, 6057, 489, 507, 2929 ],]
-        elif lig == "lig_16":
-            self.res_lst=[[ 6060, 6061, 6063, 489, 507, 2929 ],]
-
